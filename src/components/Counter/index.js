dea0e362b95af9c7828eb18d2b02ba8f365a9c67
sim89 <simrenkaursoor@gmail.com>
2022-01-12 11:31:55 +0000

    added usereducer hook

--- src/components/Counter/index.js
+++ src/components/Counter/index.js
@@ -1,25 +1,46 @@
-import React, { useState } from "react";
+import React, { useState, useReducer } from "react";
+
+const ACTIONS = { INCREMENT: "increment", DECREMENT: "decrement" };
+
+function reducer(state, action) {
+   switch (action.type) {
+      case ACTIONS.INCREMENT:
+         return {
+            count: state.count + 1,
+         };
+
+      case ACTIONS.DECREMENT:
+         return {
+            count: state.count - 1,
+         };
+      default:
+         return state;
+   }
+}
 
 function Counter() {
-  const [count, setCount] = useState(0);
+   //const [count, setCount] = useState(0);
+   const [count, dispatch] = useReducer(reducer, 0);
 
-  function incrementCount() {
-    const updatedCount = count + 1;
-    setCount(updatedCount);
-  }
+   function incrementCount() {
+      //const updatedCount = count + 1;
+      //setCount(updatedCount);
+      dispatch({ type: ACTIONS.INCREMENT });
+   }
 
-  function decrementCount() {
-    const updatedCount = count - 1;
-    setCount(updatedCount);
-  }
+   function decrementCount() {
+      //const updatedCount = count - 1;
+      //setCount(updatedCount);
+      dispatch({ type: ACTIONS.DECREMENT });
+   }
 
-  return (
-    <div>
-      <button onClick={incrementCount}>+</button>
-      <p>{count}</p>
-      <button onClick={decrementCount}>-</button>
-    </div>
-  );
+   return (
+      <div>
+         <button onClick={incrementCount}>+</button>
+         <p>{count}</p>
+         <button onClick={decrementCount}>-</button>
+      </div>
+   );
 }
 
 export default Counter;
